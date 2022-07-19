3978c52fdddf14da28797837288e4e3af11b201f
Glitch (fekabot) <none>
2022-07-19 11:16:44 +0000

    🎣🚇 Checkpoint ./komutlar/sayıoylama.js:44251422/8719

--- komutlar/sayıoylama.js
+++ komutlar/sayıoylama.js
@@ -14,96 +14,10 @@ const Discord = require('discord.js');
        .setTimestamp()
        .setFooter('', client.user.displayAvatarURL())
        .addField(`**Oylama:**`, `**${question}**`)).then(function(message) {
-         message.react('✅');
-         message.react('❌');
-       });
-     };
-     exports.conf = {
-       enabled: true,
-       guildOnly: false,
-       aliases: ['anket'],
-  permLevel: 2
-};
-exports.help = {
-  name: 'const Discord = require('discord.js');
- exports.run = (client, message, args) => {
-   message.delete();
-   let question = args.join(' ');
-   let user = message.author.username
-   if (!question) return message.channel.send(
-     new Discord.MessageEmbed()
-     .setColor("BLUE")
-     .setDescription(` Yazı yazman gerek.`)).then(m => m.delete(5000));
-     message.channel.send(
-       new Discord.MessageEmbed()
-       .setColor("BLUE")
-       .setThumbnail(client.user.displayAvatarURL())
-       .setTimestamp()
-       .setFooter('', client.user.displayAvatarURL())
-       .addField(`**Oylama:**`, `**${question}**`)).then(function(message) {
-         message.react('✅');
-         message.react('❌');
-       });
-     };
-     exports.conf = {
-       enabled: true,
-       guildOnly: false,
-       aliases: ['anket'],
-  permLevel: 2
-};
-exports.help = {
-  name: 'oylama',
-  description: 'Oylama yapmanızı sağlar.',
-  usage: 'oylama <oylamaismi>'
-};sconst Discord = require('discord.js');
- exports.run = (client, message, args) => {
-   message.delete();
-   let question = args.join(' ');
-   let user = message.author.username
-   if (!question) return message.channel.send(
-     new Discord.MessageEmbed()
-     .setColor("BLUE")
-     .setDescription(` Yazı yazman gerek.`)).then(m => m.delete(5000));
-     message.channel.send(
-       new Discord.MessageEmbed()
-       .setColor("BLUE")
-       .setThumbnail(client.user.displayAvatarURL())
-       .setTimestamp()
-       .setFooter('', client.user.displayAvatarURL())
-       .addField(`**Oylama:**`, `**${question}**`)).then(function(message) {
-         message.react('✅');
-         message.react('❌');
-       });
-     };
-     exports.conf = {
-       enabled: true,
-       guildOnly: false,
-       aliases: ['anket'],
-  permLevel: 2
-};
-exports.help = {
-  name: 'oylama',
-  description: 'Oylama yapmanızı sağlar.',
-  usage: 'oylama <oylamaismi>'
-};saconst Discord = require('discord.js');
- exports.run = (client, message, args) => {
-   message.delete();
-   let question = args.join(' ');
-   let user = message.author.username
-   if (!question) return message.channel.send(
-     new Discord.MessageEmbed()
-     .setColor("BLUE")
-     .setDescription(` Yazı yazman gerek.`)).then(m => m.delete(5000));
-     message.channel.send(
-       new Discord.MessageEmbed()
-       .setColor("BLUE")
-       .setThumbnail(client.user.displayAvatarURL())
-       .setTimestamp()
-       .setFooter('', client.user.displayAvatarURL())
-       .addField(`**Oylama:**`, `**${question}**`)).then(function(message) {
          message.react('1️⃣');
          message.react('2️⃣');
-       message.react(
+         message.react('3️⃣');
+         message.react('4️⃣');
        });
      };
      exports.conf = {
@@ -113,10 +27,7 @@ exports.help = {
   permLevel: 2
 };
 exports.help = {
-  name: 'oylama',
-  description: 'Oylama yapmanızı sağlar.',
-  usage: 'oylama <oylamaismi>'
-};sayoylama',
-  description: 'Oylama yapmanızı sağlar.',
+  name: 'sayıoylama',
+  description: 'Sayılı Oylama yapmanızı sağlar.',
   usage: 'sayıoylama <oylamaismi>'
 };

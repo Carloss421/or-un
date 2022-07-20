292b2baa9b9cc2816c040bbc3ade00f8235dbecb
Glitch (fekabot) <none>
2022-07-20 08:09:52 +0000

    💰🏣 Checkpoint ./komutlar/canlıdestek.js:44251422/6905

--- komutlar/canlıdestek.js
+++ komutlar/canlıdestek.js
@@ -1,41 +1,40 @@
 const Discord = require('discord.js');
-
+const client = new Discord.Client()
 exports.run = async(client, message, args) => {
-const emoji = message.client.emojis.get('');
       let isEnabled;
-      message.reply("`Talebiniz dikkate alındı, ekibimiz sizinle iletişim kuracaktır.`");
+      message.reply("`Birazdan Yetkili Ekibimiz Sizinle ilgilenicektir.`");
       let mesaj = args.slice(0).join(' ');
       let chan = message.channel;
-      let destekKanal = "XXX"; //Destek Kanalı ID
-      const embed = new Discord.RichEmbed()
-        .addField(' :warning: ', `Canlı Destek Talebi`)
+      let destekKanal = "503992203719213057";
+      const embed = new Discord.MessageEmbed()
+        .addField('Dikkat', `Canlı Destek Talebi`)
         .setAuthor(`${message.author.tag} (${message.author.id})`, `${message.author.avatarURL}`)
         .setColor("00ff00")
-        .addField(`Bilgiler`, `**Sunucu**: ${message.guild.name} (${message.guild.id}) \n**Kanal**: ${message.channel.name} (${message.channel.id}) \n**Talep Sahibi**: ${message.author.tag} (${message.author.id}) \n**Destek Mesajı**: ${mesaj}`)
-        .setFooter("X | Canlı Destek")
-        client.channels.get(destekKanal).send({
+        .addField(`Bilgiler`, `**Sunucu**: ${message.guild.name} (${message.guild.id}) \n**Kanal**: ${message.channel.name} (${message.channel.id}) \n**Destek İsteyen**: ${message.author.tag} (${message.author.id}) \n**Destek Mesajı**: ${mesaj}`)
+        .setFooter("© Bee | Canlı Destek")
+        client.channels.cache.get(destekKanal).send({
         embed: embed
       });
     const collector = client.channels.get(destekKanal).createCollector(message => message.content.startsWith(''), {
       time: 0
     })
-    client.channels.get(destekKanal).send('** Destek .ağrısına bağlanmak için** `bağlan` **yazınız, iptal etmek için** `iptal` **yazınız.**')
+    client.channels.get(destekKanal).send('** Destek Çağrısına Bağlanmak İçin** `bağlan` **Yazınız. İptal Etmek İçin** `iptal` **Yazınız.**')
     collector.on('message', (message) => {
       if (message.content === 'iptal') collector.stop('aborted')
       if (message.content === 'bağlan') collector.stop('success')
     })
     collector.on('end', (collected, reason) => {
-      if (reason === 'time') return message.reply('``Destek talebi zaman aşımına uğradı.``')
+      if (reason === 'time') return message.reply('``Canlı Destek Zaman Aşımına Uğradı.``')
       if (reason === 'aborted') {
-        message.reply('``Destek talebi reddedildi.``')
-        client.channels.get(destekKanal).send('``Destek talebi reddedildi.``')
+        message.reply('``Canlı Destek Talebi Reddedildi.``')
+        client.channels.get(destekKanal).send('``Canlı Destek Başarıyla Reddedildi.``')
       }
       if (reason === 'success') {
-        client.channels.get(destekKanal).send('``Talebiniz dikkate alınmıştır.``')
-        client.channels.get(destekKanal).send('**Destek talebini iptal etmek için `iptal` yazınız.**)
+        client.channels.get(destekKanal).send('``Canlı Destek Talebi Alındı.``')
+        client.channels.get(destekKanal).send('**Canlı Destek Talebini İptal Etmke İçin ``iptal`` Yazınız.**')
         chan.send(`${message.author}`)
-        chan.send('``Destek talebiniz yetkili tarafından kabul edildi``')
-        chan.send('**Destek talebini iptal etmek için `iptal` yazınız.**')
+        chan.send('``Destek Talebiniz Yetkili Tarafından Kabul Edildi``')
+        chan.send('**Destek Talebini İptal Etmek İçin `iptal` Yazınız.**')
         isEnabled = true
         client.on('message', message => {
           function contact() {
@@ -43,8 +42,8 @@ const emoji = message.client.emojis.get('');
             if (message.author.id === client.user.id) return
             if (message.content.startsWith('iptal')) {
               message.channel.send('``Canlı Destek İptal Edildi``')
-              if (message.channel.id === chan.id) client.channels.get(destekKanal).send('``Canlı destek iptal edildi.``')
-              if (message.channel.id === destekKanal) chan.send('``Canlı destek iptal edildi.``')
+              if (message.channel.id === chan.id) client.channels.get(destekKanal).send('``Canlı Destek Karşı Tarafdan İptal Edildi``')
+              if (message.channel.id === destekKanal) chan.send('``Canlı Destek Karşı Tarafdan İptal Edildi``')
 
               return isEnabled = false
             }
@@ -60,7 +59,7 @@ const emoji = message.client.emojis.get('');
   exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["canlı","canli","destek"],
+  aliases: [],
   permLevel: 0
 };
 

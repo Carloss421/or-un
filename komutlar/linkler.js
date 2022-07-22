b74d4976faa0e0fc982f2d6dbd629e783cee0b0e
Glitch (fekabot) <none>
2022-07-22 10:42:19 +0000

    👆😼 Checkpoint ./komutlar/düello.js:44251422/4311 ./komutlar/mcödül.js:44251422/1017 ./komutlar/mcsunucubilgi.js:44251422/1387 ./komutlar/linkler.js:44251422/766

---
+++ komutlar/linkler.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+exports.run = (client, message, args) => {
+  
+    const yenilikler = new Discord.MessageEmbed()
+    .setTitle("Rise Network İnternet Hizmetleri")
+    .setThumbnail(client.user.displayAvatarURL())
+    .setColor("BLUE")
+    .setDescription("**Youtube - Diji: https://youtube.com/dijitalaile \n Youtube - DijiGame : https://youtube.com/dijigame \n Discord https://discord.gg/DMgPJDNTVc **")
+    .setTimestamp()
+    message.channel.send(yenilikler) 
+  
+};
+
+exports.conf = {
+  aliases: ['linkler']
+};
+
+exports.help = {
+  name: 'linkler'
+};

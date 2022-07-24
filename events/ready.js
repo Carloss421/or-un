5ade20726f204f7f199aa46b781ec3a54c7eef12
Glitch (fekabot) <none>
2022-07-24 14:08:05 +0000

    🌎🐁 Checkpoint ./komutlar/şifreoluştur.js:44251422/732 ./package.json:44251422/2533 ./komutlar/sürelirol.js:44251422/1756 ./komutlar/kurallar.js:44251422/1202 ./events/ready.js:44251422/1868

--- events/ready.js
+++ events/ready.js
@@ -3,19 +3,21 @@ const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
-
 module.exports = client => {
- setInterval(function() {
-}, 8000);
-  var msgArray = [
-"@bi ile sunucularınız güvende.",
-"DijitalAile discord sunucumuza beklerim",
-"Her zaman sizlerle!"
- ];
-
- setInterval(() => {
-  var rastgeleOyun = Math.floor(Math.random() * msgArray.length);
-  client.user.setActivity(`!yardım`, { type: 'PLAYING' ,  })
-}, 5000);
-    console.log(`Asreaper başarıyla giriş yaptı.`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+   var oyun = [
+        "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
+        "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
+        "💪 7/24 Aktif!",  
+        "💡 g!davet | Botumuzu ekleyin",
+        "👨 35.000 Kullanıcı!",
+        "🌍 115 Sunucuda Hizmet!",
+        "g!yardım 🔥 + g!davet 🔥 + g!otorol"
+    ];
+    setInterval(function() {
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+        client.user.setGame(oyun[random], "https://twitch.tv/dijitalailee");
+        }, 2 * 2500);
 }

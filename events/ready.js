b71ee3b881a80a90dda9eb881553407e291fcded
Glitch (fekabot) <none>
2022-07-24 14:32:11 +0000

    🧀🏝 Checkpoint ./events/ready.js:44251422/2037

--- events/ready.js
+++ events/ready.js
@@ -2,23 +2,24 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+
 var prefix = ayarlar.prefix;
+
 module.exports = client => {
-console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-client.user.setStatus("STREAMING");
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+   var oyun = [
+        "@bi ile sunucularınız güvende.",
+        "DijitalAile discord sunucumuza gelmeyi unutmayın!",
+        "Her zaman sizlerle!"
 
-var oyun = [
-"@bi ile sunucularınız güvende.",
-"DijitalAile discord sunucumuza gelmeyi unutmayın!",
-"Her zaman sizlerle!"
-];
+    ];
 
-setInterval(function() {
+    setInterval(function() {
 
-var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-client.user.setPresence()(oyun[random], "https://twitch.tv/dijitalailee");
-}, 2 * 2500);
-
+        client.user.setActivity()(oyun[random], "https://twitch.tv/dijitalailee");
+        }, 2 * 2500);
 }

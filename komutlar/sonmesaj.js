95ba94a3b525d56bb2a129fcc98f5efb52c6fcb0
Glitch (fekabot) <none>
2022-07-26 16:00:30 +0000

    🏡🚤 Checkpoint ./bot.js:44251422/2836 ./komutlar/sonmesaj.js:44251422/5596

--- komutlar/sonmesaj.js
+++ komutlar/sonmesaj.js
@@ -1,18 +1,35 @@
-const discord = require('discord.js');
+const Discord = require('discord.js')
+const db = require('quick.db')
+
 exports.run = async (client, message, args) => {
-  const wembed = new discord.MessageEmbed()
-  .setColor("BLUE")
-  .setTimestamp()
-  .setDescription(`https://discord.com/channels/${message.guild.id}/${message.author.lastMessageChannelID}/${message.author.lastMessageID}`)
-  message.channel.send(wembed)
+    const data = await db.fetch(`snipe.id.${message.guild.id}`)
+    if(!data) {
+    const snipe2 = new Discord.RichEmbed()
+  .setAuthor(client.user.username, client.user.avatarURL)
+  .setDescription(`Hiç mesaj silinmemiş.`)
+.setColor(`#f3c7e1`)
+    message.channel.send({embed: snipe2});
+          } else {
+  let kullanıcı = client.users.cache.get(data);
+  const data2 = await db.fetch(`snipe.mesaj.${message.guild.id}`)
+  const snipe = new Discord.MessageEmbed()
+  .setAuthor(kullanıcı.username, kullanıcı.avatarURL)
+  .setDescription(`En son silinen mesaj: ` + data2)
+.setColor(`#f3c7e1`)
+  message.channel.send(snipe) }
 }
+
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ["son-mesaj"],
-  permLevel: 0
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: 0
 }
 
 exports.help = {
-  name: 'sonmesaj'
+ name: 'sonmesaj',
+ description: `Son silinen mesajı size gösterir.`,
+ usage: 'sonmesaj',
+ kategori: '**EĞLENCE**',
+ permLvl: '**HERKES KULLANABİLİR**'
 };

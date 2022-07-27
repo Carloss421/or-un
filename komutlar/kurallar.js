b7faad7974c0056fd181429109bd412af8be088d
Glitch (fekabot) <none>
2022-07-27 20:20:09 +0000

    🎥🏑 Checkpoint ./komutlar/adamasmaca.js:47164045/3673 ./komutlar/kurallar.js:47164045/3542

--- komutlar/kurallar.js
+++ komutlar/kurallar.js
@@ -1,43 +1,45 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-exports.run = async(client, message) => {
 
-    const rules = new Discord.MessageEmbed()
-    
-      .setColor('RANDOM')
-      .addField(`Kurallar`, [`
-      
-      - Küfür, argo ve reklam kesinlikle **yasaktır**!
-      - Spam ve flood **yasaktır**!
-      - Din, ırk ve siyaset konuşmak **yasaktır**!
-      - Herkes birbirine saygılı olmalıdır!
-      - Herkesin görüşü kendinedir bu yüzden tartışmak **yasaktır**!
-      - Üstünlük göstermek/taslamak **yasaktır**!
-      - Caps ve emoji kullanımı **yasaktır**! (Aşırı Olmadıkça)
-      - Oynuyor kısmına reklam, küfür koymak **yasaktır**!
-      - Yetkililere karşı gelmek **yasaktır**!
-      - Bot basmak/j4j yapmak **yasaktır**!
-      - Rahatsızlık vermek **yasaktır**! \n\n
-      - SUNUCUYA GİRDİĞİNİZ ANDA OKUMUŞ SAYILACAKSINIZ!
+exports.run = (client, message, args) => {
+  const kurallar = new Discord.MessageEmbed()
+    .setDescription(`**SUNUCU KURALLARI**
 
-      `])
 
-       message.delete();
-      //message.react("?");
+- **Tehdit edici küfürler, argo, kötü söz, nefret söylemi içeren** sunucular başlatma, bu sunucuları destekleme veya koordine etme. Bir kişiye veya **topluluğa ırk, etnik köken, ulusal köken, cinsiyet, toplumsal cinsiyet, cinsel yönelim, din veya engel** gibi niteliklerinden ötürü saldırıda bulunmak yasaktır.
+- Yöneticilerin, yetkililerin ve sunucu üyelerinin **onurunu kırmak, onu küçük düşürmek** gibi eylemler yasaktır.
+- Mesaj yazarken **büyük harf** kullanmak **bağırmak** anlamına geleceği için kesinlikle büyük harf kullanarak yazı yazmayınız.
+- Discord sohbet merkezi olduğu için gereksiz kişisel tartışmalara ve atışmalara girmek yasaktır.
+- Başkasına ait kişisel bilgiler yayınlamak yasaktır.
+- Yetkililer her yazılan mesajı gözden kaçırabilir, sizden ricamız lütfen böyle sorunları yetkilileri etiketleyerek bildiriniz.
+- Çekiliş, kanal, yayıncı ve sunucu reklamı yapmak yasaktır sadece reklam kanalında kendi sunucunuzun reklamını yapabilirsiniz.
+- Oyun hesabı, hediyelik eşya, oyun ekipmanı vb. şeylerin satışı/takası ve referans linkleri paylaşımı ve dilenmek yasaktır sadece satış  kanallarında yapılmaktadır.
+- İllegal, yasa dışı sayılacak işler yasaktır.
+- Uygunsuz kullanıcı adı ve profil fotoğrafı koymak yasaktır.
+- Oyunlarda hile kullanarak sunucu üyelerini rahatsız etmek yasaktır.
+- Sunucumuzdaki oyun odalarını kullanmayıp adam çekmeye çalışmak yasaktır.
+- **Aşağılamak, küçük düşürmek, ifşalamak** yasaktır.
+- Fotoğraf & video kanallarına **+18, pornografik** içerikler atmak yasaktır.
+- **Spoiler** mesajı atmak ve spoiler vermek kesinlikle yasaktır. Sadece spoiler kanalına spoiler verilebilir.
 
-    return message.channel.send(rules).then(keleS => keleS.react("?"));
 
-}
+- ** Discord Topluluk kurallarına uymadığınız sürece sunucumuzdan kalıcı olarak banlanacaksınız.**
+• Discord Topluluk kuralları hakkında daha fazla bilgi almak istiyorsanız, https://discord.com/terms & https://discord.com/guidelines sitesini bir gözden geçirin.
+
+
+• Klasik Discord sunucu kurallarına uymanız yeterli olacaktır.`)
+    .setColor("RANDOM")
+    .setFooter(`${message.guild.name}`)
+  message.channel.send(kurallar);
+};
+
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['rules'],
-    permLevel: 0
-}
-
+  enabled: true,
+  guildOnly: false,
+  aliases: [`kurallar`],
+  permLevel: 0
+};
 exports.help = {
-    name : 'kurallar',
-    description: 'Hazır kuralları kanalınıza atar.',
-    usage: 'kurallar'
+  name:"rules"
 }

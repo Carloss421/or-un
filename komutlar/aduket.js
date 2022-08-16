0bea50c67fab84f18486d4eb2dfde074348761e6
Glitch (abibot2) <none>
2022-08-16 09:25:14 +0000

    🐄👜 Checkpoint ./komutlar/aduket.js:42526356/589

--- komutlar/aduket.js
+++ komutlar/aduket.js
@@ -16,14 +16,8 @@ const EmbedFwhyCode = new Discord.MessageEmbed()
       `${FwhyCode} ` + `**${message.author.username}** Sana Aduket Çekti Vay Yavşik Vay 😄`
     )
     .setImage(
-      "https://korkubilimi.com/wp-content/uploads/2017/12/aduket-e1514309463438.jpg",
-      "https://cdn.discordapp.com/attachments/935610123869634561/1009020320277073980/images_48.jpeg",
-      "https://cdn.discordapp.com/attachments/935610123869634561/1009020566323351564/images_49.jpeg",
-      "https://cdn.discordapp.com/attachments/935610123869634561/1009020716663980094/de886cs-da1ac0c7-47d3-4a06-a876-9c31ad22e4f5.png",
-      "https://cdn.discordapp.com/attachments/935610123869634561/1009020847027130388/images_50.jpeg",
-      "https://cdn.discordapp.com/attachments/935610123869634561/1009021066527645766/dhalsim_1256975.gif"
+      "https://korkubilimi.com/wp-content/uploads/2017/12/aduket-e1514309463438.jpg"
     ) 
-    .setImage()
     .setFooter(client.user.username + " Sundu", client.user.avatarURL)
     .setTimestamp();
 

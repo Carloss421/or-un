86f4f23279ee3ebca3b994fa0db138b134923f1d
Glitch (abibot2) <none>
2022-08-23 11:24:21 +0000

    🌚🌞 Checkpoint ./komutlar/yardımbuton.js:47717818/12836

--- komutlar/yardımbuton.js
+++ komutlar/yardımbuton.js
@@ -1,20 +1,25 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 const categorylist = require("fs").readdirSync("./komutlar/").filter(s => s !== "private");
   for (const category of categorylist) {
 const Discord = require('discord.js');
 const { MessageButton } = require('discord-buttons');
 
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
 exports.run = async (client, message, args) => {
 
-  const buttonDelete = new MessageButton().setStyle('red').setLabel('♻Temizle♻').setID('buttonDelete')
+  const buttonDelete = new MessageButton().setStyle('red').setLabel('Clear').setID('buttonDelete')
 
+  // // // // //
 
   let embed = new Discord.MessageEmbed()
-  .setColor('WHITE')
-  .setThumbnail("https://cdn.discordapp.com/attachments/999741382841344160/1000457835387301909/standard.gif")
-  .setDescription(`arty development`)
-  .setImage("https://cdn.discordapp.com/attachments/910809722477744161/916700535082610688/PicsArt_11-28-01.59.59.jpg")
-  message.channel.send(embed, {buttons: [new MessageButton().setStyle('green').setLabel('1️⃣').setID('1'), new MessageButton().setStyle('blurple').setLabel('2️⃣').setID('2'), new MessageButton().setStyle('blurple').setLabel('3️⃣').setID('3'), buttonDelete]}).then(async function(helpMessage) {
+  .setColor('#000001')
+  .setTitle('@bi Yardım menüsü')
+  .addField(`Komutlar`, `Alttaki butonlara basarak tüm komutları öğrenebilirsiniz.`)
+  .setDescription(`Dijitalaile - @bi`)
+  message.channel.send(embed, {buttons: [new MessageButton().setStyle('green').setLabel('1').setID('1'), new MessageButton().setStyle('blurple').setLabel('2').setID('2'), new MessageButton().setStyle('blurple').setLabel('3').setID('3'), buttonDelete]}).then(async function(helpMessage) {
 
     helpMessage.createButtonCollector(user => user.clicker.user.id == message.author.id).on('collect', async (button) => {
 
@@ -26,44 +31,32 @@ exports.run = async (client, message, args) => {
 
       } else if (button.id == '1') {
 
-        embed.setTitle('Sayfa-1 arty')
-        embed.setThumbnail("https://cdn.discordapp.com/attachments/910809722477744161/916700535082610688/PicsArt_11-28-01.59.59.jpg")
-        embed.setDescription(`arty development`)
-  
-  
- 
-        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('green').setLabel('1️⃣').setID('1'), new MessageButton().setStyle('blurple').setLabel('2️⃣').setID('2'), new MessageButton().setStyle('blurple').setLabel('3️⃣').setID('3'),  buttonDelete]})
+        embed.setTitle('Sayfa 1')
+        embed.setDescription(`Sayfa 1`)
+        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('green').setLabel('1').setID('1'), new MessageButton().setStyle('blurple').setLabel('2').setID('2'), new MessageButton().setStyle('blurple').setLabel('3').setID('3'), buttonDelete]})
 
         button.reply.defer()
 
       } else if (button.id == '2') {
 
-        embed.setTitle('Sayfa-2 arty')
-        embed.setDescription(`arty development`)
-
-        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('blurple').setLabel('1️⃣').setID('1'), new MessageButton().setStyle('green').setLabel('2️⃣').setID('2'),new MessageButton().setStyle('blurple').setLabel('3️⃣').setID('3'), buttonDelete]})
+        embed.setTitle('Sayfa 2')
+        embed.setDescription(`Sayfa 2`)
+        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('blurple').setLabel('1').setID('1'), new MessageButton().setStyle('green').setLabel('2').setID('2'), new MessageButton().setStyle('blurple').setLabel('3').setID('3'), buttonDelete]})
 
         button.reply.defer()
 
       } else if (button.id == '3') {
 
-        embed.setTitle('Sayfa-3 arty')
-        embed.setColor('WHITE')
-        embed.setColor('WHITE')
-        embed.setDescription(`arty development`)
- 
-        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('blurple').setLabel('1️⃣').setID('1'), new MessageButton().setStyle('blurple').setLabel('2️⃣').setID('2'), new MessageButton().setStyle('green').setLabel('3️⃣').setID('3'), buttonDelete]})
+        embed.setTitle('Sayfa 3')
+        embed.setDescription(`Sayfa 3`)
+        helpMessage.edit(embed, {buttons: [new MessageButton().setStyle('blurple').setLabel('1').setID('1'), new MessageButton().setStyle('blurple').setLabel('2').setID('2'), new MessageButton().setStyle('green').setLabel('3').setID('3'), buttonDelete]})
 
         button.reply.defer()
-  
-      }
+      };
     });
   });
 };
 }
-
-exports.config = {
-    name: "yardım1",
-    guildOnly: true,
-    aliases: ["yardım11"],
-  };
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+exports.conf = {"aliases": ["y1"]}
+exports.help = {"name":"button"}
